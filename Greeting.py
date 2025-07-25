0315239810a0cb2d91ec1634c69605098d56c1e2
Mwathi Margaret <mwathiann201@yahoo.com>
2025-07-25 23:20:07 +0300

    Added weekly challenge files

---
+++ Greeting.py
@@ -0,0 +1,4 @@
+name =  input("Enter your user's name: ")
+color =  input("Enter your favorite color: ")
+print(f"Hello, {name}! Your favorite color, {color}, is awsome!")
+
---
+++ Variables.py
@@ -0,0 +1,17 @@
+# site_name ="Power Learn Project"
+# print(site_name)
+
+# site_name = "I Love coding"
+# print(site_name)
+
+# a,b,c = 5,4,"Hello World"
+# print(a)
+# print(b)
+# print(c)
+
+# fruits = ["Mango","Apple","Banana", 24]
+# print(fruits)
+# print(fruits[1])
+
+Capital_city = {"Kenya": "Nairobi", "Nigeria":"Lagos"}
+print(Capital_city[1])
